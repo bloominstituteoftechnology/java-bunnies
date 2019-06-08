9e2e9836c6a6afdb873f20c8b8a4a0a6aa3d62fe
jrmmba8314 <jrmmba@outlook.com>
2019-06-07 19:10:04 -0700

    Updating Version

--- bunnies/src/main/java/com/lambdaschool/bunnies/controller/BunniesController.java
+++ bunnies/src/main/java/com/lambdaschool/bunnies/controller/BunniesController.java
@@ -1,21 +1,19 @@
 package com.lambdaschool.bunnies.controller;
 
-        import com.lambdaschool.bunnies.BunniesApplication;
-        import com.lambdaschool.bunnies.exception.ResourceNotFoundException;
-        import com.lambdaschool.bunnies.model.Bunny;
-        import org.slf4j.Logger;
-        import org.slf4j.LoggerFactory;
-        import org.springframework.beans.factory.annotation.Autowired;
-        import org.springframework.http.HttpStatus;
-        import org.springframework.http.ResponseEntity;
-        import org.springframework.web.bind.annotation.GetMapping;
-        import org.springframework.web.bind.annotation.PathVariable;
-        import org.springframework.web.bind.annotation.RequestMapping;
-        import org.springframework.web.bind.annotation.RestController;
-        import org.springframework.web.servlet.ModelAndView;
+import com.lambdaschool.bunnies.BunniesApplication;
+import com.lambdaschool.bunnies.exception.ResourceNotFoundException;
+import com.lambdaschool.bunnies.model.Bunny;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
-        import javax.servlet.http.HttpServletRequest;
-        import java.util.ArrayList;
+import javax.servlet.http.HttpServletRequest;
 
 @RestController
 @RequestMapping("/data")
